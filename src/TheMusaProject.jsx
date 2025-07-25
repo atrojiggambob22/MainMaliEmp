dbc0ca0399436a2d065378ce19b75fa374b5eef3
Atrojiggambob <mathiasonyebuchi13@gmail.com>
2025-07-25 19:59:09 +0100

    added animation to the musa page

--- src/TheMusaProject.jsx
+++ src/TheMusaProject.jsx
@@ -8,6 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faBars, faXmark, faDrum, faBuilding, faCrown, faTicket, faPanorama, faBookAtlas, faBook, faLocation, faLocationArrow, faCalendar, faMobile, faMailBulk, faWebAwesome, faGlobe} from '@fortawesome/free-solid-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faOptinMonster } from '@fortawesome/free-brands-svg-icons/faOptinMonster';
+import SlideIn from './SlideIn';
+import FadeInSection from "./FadeInSection";
 export default function TheMusaProject(){
      const navigate = useNavigate();
     const goHome = () => {
@@ -83,10 +85,17 @@ const gotoTheMusaProject = () => {
             </nav>
 
             <div className='FirstMaliSSsub3-Sub2-Content1 FDrum1'>
+
+            <FadeInSection direction="top" delay={0.4}>
+
             <h3 className='text-[#E6C157] text-[50px] font-bold text-center'>The Musa Project</h3>
+            </FadeInSection>
+            <FadeInSection direction="bottom" delay={0.4}>
+
             <p className='Firtsparolunderthecontent1 firstppogthhmusa firstsegofheadd'>
             "Celebration is a stratejy. Joy is Political"
             </p>
+            </FadeInSection>
            
         </div>
 
@@ -95,46 +104,77 @@ const gotoTheMusaProject = () => {
             <div className="flex flex-col relative left-[25%] mt-[50px] thefisrtsideofmusa">
 
             <div className="firstofthemh3333 my-[70px] inline-block w-[40%] relative left-[50px]">
-                <h3 className="inline-block text-[30px] font-bold  relative left-[20px]">Rooted in Our Rythyms</h3></div>
-            <p className="w-[50%] ">The Musa Project is rooted in a longing to reconnect African cities- especially in West Africa-to their ancestral rhythms and cultural integrity. In a world where globalization often dilutes local traditions, we believe that celebrating our heritage is not just an act of remembrance, but a powerful strategy for cultural resilience and community building.</p>
+                <SlideIn direction="top" delay={0.8}>
+
+                <h3 className="inline-block text-[30px] font-bold  relative left-[20px]">Rooted in Our Rythyms</h3>
+                </SlideIn>
+
+                </div>
+        <FadeInSection direction="left" delay={0.9}>
+
+            <p className="w-[50%] font-light ">The Musa Project is rooted in a longing to reconnect African cities- especially in West Africa-to their ancestral rhythms and cultural integrity. In a world where globalization often dilutes local traditions, we believe that celebrating our heritage is not just an act of remembrance, but a powerful strategy for cultural resilience and community building.</p>
+        </FadeInSection>
 
             </div>
             
-            <div className="flex text-left gap-[60px] relative left-[20%] my-[10px] mb-[10%]  py-[90px] firstofthemh3333causi">
-                <div className="bg-[#facd659c] inline-block p-[30px]  w-[30%] rounded-[20px] firstofthemh3333causi-sub1" >
+            <div className="flex text-left gap-[30px] relative left-[10%] my-[10px] mb-[10%] mx-[60px]  py-[90px] firstofthemh3333causi">
+                <FadeInSection direction="left" delay={0.8}>
+
+                <div className="bg-[#facd659c] inline-block p-[30px]  w-[50%] rounded-[20px] firstofthemh3333causi-sub1" >
                     <h3 className="my-[10px] text-red-900 font-bold text-[20px] ">CULTURAL RESILIENCE</h3>
                     <p className="w-[90%]" >Our festivals serve as living libraries,
                         preserving ancient wisdom while creating space for contemporary  expression and innovation </p>
                 </div>
-                <div className="bg-[#facd659c] inline-block p-[30px]  w-[30%] rounded-[20px] firstofthemh3333causi-sub1">
+                </FadeInSection>
+                <FadeInSection direction="right" delay={0.9}>
+
+                <div className="bg-[#facd659c] inline-block p-[30px]  w-[50%] rounded-[20px] firstofthemh3333causi-sub1">
                     <h3 className="my-[10px]  text-red-900 font-bold text-[20px] ">COMMUNITY BUILDING</h3>
                     <p className="w-[90%]">Through shared celebration, we strengthen the bomd that connect's us accross the generation, creating unity in our diversity</p>
                 </div>
+                </FadeInSection>
+
+
             </div>
 
 
         </div>
 
             <div className=" py-[50px]  bg-[#d2960b9c] text-center ">
+                <FadeInSection direction="top" delay={0.5}>
+
                 <div className="firstsegofheadd firstsegofheaddh33head"><h3 className="text-[40px] font-bold">Our Vision</h3></div>
+
+                </FadeInSection>
+
                 <div className="grid grid-cols-3 my-[90px] thefisrtsideofmusa-minicos ">
+                    <FadeInSection direction="left" delay={0.4}>
+
                     <div>
                         <div className="icon rounded-[50%] bg-yellow-600 inline-block p-[20px] px-[25px]"  >🏛️</div>
                         <h3 className="font-bold text-[20px]">PRESERVE HERITAGE</h3>
                         <p className="w-[70%] relative left-[20%] my-[20px]"> Safeguarding the rich cultural traditions of the Mali Empire for future generations
                         through immersive experiences. </p>
                     </div>
+
+                    </FadeInSection>
+                    <FadeInSection direction="right" delay={0.6}>
+
                     <div>
                         <div  className="icon rounded-[50%] bg-red-800 inline-block p-[20px] px-[25px]">🌍</div>
                         <h3 className="font-bold text-[20px]">CONNECT COMMUNITIES</h3>
                         <p className="w-[70%] relative left-[20%] my-[20px]">Bullding briages between Alrican diaspora communues worawide through shared cultural celebration</p>
                     </div>
+                    </FadeInSection>
+                    <FadeInSection direction="bottom" delay={0.4}>
+
                     <div>
                         <div  className="icon rounded-[50%] bg-gray-800 inline-block p-[20px] px-[25px]">✨</div>
                         <h3 className="font-bold text-[20px]">INSPIRE INNOVATION</h3>
                         <p className="w-[70%] relative left-[20%] my-[20px]">Blending ancient wisdom with Arroruturist
 vision to credle lew torts or cultural expression</p>
                     </div>
+                    </FadeInSection>
                 </div>
             </div>
 
